3141e449563bfaa86df41bd2f384b2e83d6c5fc4
edimmfs <e21211995@gmail.com>
2022-06-12 21:18:57 +0300

    first commit

---
+++ src/components/sign-up-form/sign-up-from.component.jsx
@@ -0,0 +1,92 @@
+import { useState } from "react"
+import { Alert } from "react-bootstrap";
+import FormInput from '../../components/form-input/form-input.component'
+
+import {createAuthUserWithEmailAndPassword, createUserDocumentFromAuth} from '../../utils/firebase/firebase.utils'
+import './sign-up-form.styles.scss'
+import Button from '../button/button.component'
+
+
+const defaultFormFields = {
+    displayName:'',
+    email:'',
+    password:'',
+    confirmPassword:'',
+}
+
+
+const SignUpForm = () => {
+
+    
+    const [formFields, setFormFields] = useState(defaultFormFields);
+    const {displayName, email, password, confirmPassword} = formFields;
+    
+    console.log(formFields)
+
+    const resetFormFields = () => {
+        setFormFields(defaultFormFields);
+    }
+
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+
+
+        if(formFields.password !== formFields.confirmPassword){
+            alert("passwords do not match");
+            return;
+        }
+
+        
+        try{
+            const {user} = await createAuthUserWithEmailAndPassword(email, password);
+            
+            await createUserDocumentFromAuth(user, {displayName});
+            resetFormFields();
+        } 
+        
+        catch(error){
+            if(error.code === "auth/email-already-in-use"){
+                alert("Cannot create user, email already in use")
+            }else{
+                console.log("user creation encountered an error",error);
+            }
+                
+        }
+
+    }
+
+
+
+    const handleChange = (event) => {
+        
+        const {name, value} = event.target;
+
+
+        setFormFields({...formFields,[name]:value})
+
+    }
+
+    return(
+        <div className="sign-up-container">
+            <h2>Don't have an account?</h2>
+            <span> Sign up with your email and password </span>
+            <form onSubmit={handleSubmit}>
+                
+                <FormInput label="Display Name" type="text" name='displayName' required onChange={handleChange} value={displayName}/>
+
+                
+                <FormInput label="Email" type="email" name='email' required onChange={handleChange} value={email}/>
+
+                
+                <FormInput label="Password" type="password" name='password' required onChange={handleChange} value={password}/>
+
+                
+                <FormInput label="Confirm Password" type="password" name='confirmPassword' required onChange={handleChange} value={confirmPassword}/>
+
+                <Button  type="submit" >Sign Up</Button>
+            </form>
+        </div>
+    )
+}
+
+export default SignUpForm
---
+++ src/routes/sign-in/sign-in.component.jsx
@@ -0,0 +1,32 @@
+import React,{useEffect} from 'react'
+import {auth, signInWithGooglePopup, createUserDocumentFromAuth,  } from '../../utils/firebase/firebase.utils'
+import SignUpForm from '../../components/sign-up-form/sign-up-from.component'
+
+const SignIn = () => {
+
+   
+
+    const logGoogleUser = async () => {
+        const {user} = await signInWithGooglePopup();
+        
+        const userDocRef = await createUserDocumentFromAuth(user)
+    }
+   
+
+
+    
+  return (
+    <div>
+        <h1>sign-in page</h1>
+        <button onClick={logGoogleUser}>
+            Sign in with Google Popup
+        </button>
+        <SignUpForm/>
+        
+        
+
+    </div>
+  )
+}
+
+export default SignIn
