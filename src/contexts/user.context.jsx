b078a0d6315290fbb2a80cbd2d048a7c8a3761df
edimmfs <e21211995@gmail.com>
2022-07-18 14:07:31 +0300

    added reducers to cart context

--- src/contexts/user.context.jsx
+++ src/contexts/user.context.jsx
@@ -1,5 +1,6 @@
 import React, { createContext, useState, useEffect, useReducer} from "react";
 import {onAuthStateChangedListener , createUserDocumentFromAuth} from '../utils/firebase/firebase.utils'
+import { createAction } from "../utils/reducer/reducer.utils";
 //actual value
 export const UserContext = createContext({
   currentUser: null,
@@ -40,7 +41,7 @@ export const UserProvider = ({children}) => {
   
 
   const setCurrentUser = (user) => {
-    dispatch({ type: USER_ACTION_TYPES.SET_CURRENT_USER, payload: user})
+    dispatch( createAction(USER_ACTION_TYPES.SET_CURRENT_USER, user))
   }
 
   const value = {currentUser, setCurrentUser}
