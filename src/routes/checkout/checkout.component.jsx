addd71e2ea2f09291db1e8325c91f267e4c0cd19
edimmfs <e21211995@gmail.com>
2022-08-09 00:33:45 +0300

    finished migrating cart context to redux

--- src/routes/checkout/checkout.component.jsx
+++ src/routes/checkout/checkout.component.jsx
@@ -1,6 +1,7 @@
-import { useContext } from 'react';
+import { useSelector} from 'react-redux'
+import {selectCartItems, selectCartTotal} from '../../store/cart/cart.selector';
 
-import { CartContext } from '../../contexts/cart.context';
+
 
 import CheckoutItem from '../../components/checkout-item/checkout-item.component';
 
@@ -12,7 +13,8 @@ import {
 } from './checkout.styles';
 
 const Checkout = () => {
-  const { cartItems, cartTotal } = useContext(CartContext);
+  const cartItems = useSelector(selectCartItems);
+  const cartTotal = useSelector(selectCartTotal);
 
   return (
     <CheckoutContainer>
--- src/store/cart/cart.action.js
+++ src/store/cart/cart.action.js
@@ -1,6 +1,5 @@
-import {CART_ACTION_TYPES} from './cart.types';
 import { createAction } from '../../utils/reducer/reducer.utils';
-
+import { CART_ACTION_TYPES } from './cart.types';
 
 const addCartItem = (cartItems, productToAdd) => {
   const existingCartItem = cartItems.find(
@@ -40,23 +39,20 @@ const removeCartItem = (cartItems, cartItemToRemove) => {
 const clearCartItem = (cartItems, cartItemToClear) =>
   cartItems.filter((cartItem) => cartItem.id !== cartItemToClear.id);
 
+export const addItemToCart = (cartItems, productToAdd) => {
+  const newCartItems = addCartItem(cartItems, productToAdd);
+  return createAction(CART_ACTION_TYPES.SET_CART_ITEMS, newCartItems);
+};
 
+export const removeItemFromCart = (cartItems, cartItemToRemove) => {
+  const newCartItems = removeCartItem(cartItems, cartItemToRemove);
+  return createAction(CART_ACTION_TYPES.SET_CART_ITEMS, newCartItems);
+};
 
-export const setIsCartOpen = (boolean) => 
+export const clearItemFromCart = (cartItems, cartItemToClear) => {
+  const newCartItems = clearCartItem(cartItems, cartItemToClear);
+  return createAction(CART_ACTION_TYPES.SET_CART_ITEMS, newCartItems);
+};
+
+export const setIsCartOpen = (boolean) =>
   createAction(CART_ACTION_TYPES.SET_IS_CART_OPEN, boolean);
-
-
-  export const addItemToCart = (cartItems, productToAdd) => {
-    const newCartItems = addCartItem(cartItems, productToAdd);
-    return createAction(CART_ACTION_TYPES.SET_CART_ITEMS ,newCartItems);
-  };
-
-  export const removeItemToCart = (cartItems, cartItemToRemove) => {
-    const newCartItems = removeCartItem(cartItems, cartItemToRemove);
-    return createAction(CART_ACTION_TYPES.SET_CART_ITEMS ,newCartItems);
-  };
-
-  export const clearItemFromCart = (cartItems, cartItemToClear) => {
-    const newCartItems = clearCartItem(cartItems, cartItemToClear);
-    return createAction(CART_ACTION_TYPES.SET_CART_ITEMS ,newCartItems);
-  };
