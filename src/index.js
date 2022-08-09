33f49b460eb99fa8b40845dcf03202a21ba03ee0
edimmfs <e21211995@gmail.com>
2022-08-09 21:30:01 +0300

    installed redux persist and using it to save items added to the shop cart

--- src/index.js
+++ src/index.js
@@ -4,10 +4,10 @@ import './index.scss';
 import App from './App';
 import {Provider} from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
+import {PersistGate} from 'redux-persist/integration/react'
 
 
-
-import {store} from './store/store.js'
+import {store, persistor} from './store/store.js'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
@@ -15,9 +15,11 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
-        <App /> 
-      </BrowserRouter>
+      <PersistGate persistor={persistor}>
+        <BrowserRouter>
+          <App /> 
+        </BrowserRouter>
+      </PersistGate>
     </Provider> 
   </React.StrictMode>
 );
